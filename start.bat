adbdd2647c911f53f7a3a2475643b33394c24048
iiTzArcur <zefanjaj@gmail.com>
2023-09-10 16:07:35 +0200

    add example bat file

---
+++ start.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET token=DISCORDTOKEN
+SET name=SUPER@ [SiC] S1
+SET game=bf2
+SET lang=en-us
+SET minplayeramount=20
+SET prevrequestcount=5
+SET channel=0
+SET startedamount=50
+
+discord_bot.exe
