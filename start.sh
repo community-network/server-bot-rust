fa1b434343e40e5a97b34c7650c2ecb528eacb65
iiTzArcur <zefanjaj@gmail.com>
2024-08-11 13:26:04 +0200

    add start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+export token=TOKEN
+export name=SERVERNAME
+export lang=en-us
+export minplayeramount=20
+export prevrequestcount=5
+export channel=0
+export startedamount=50
+export game=tunguska
+export platform=pc
+./discord_bot
